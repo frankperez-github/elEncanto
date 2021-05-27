9a8c90013de0ba188d2e6070606c93f91fb974e5
Frank Pérez <fp848584@gmail.com>
2021-05-27 19:44:48 -0400

    Background Solid

--- pages/PresentationDesk.jsx
+++ pages/PresentationDesk.jsx
@@ -27,7 +27,7 @@ function Pallets (){
                     
                     
                     <div className="StaticTruck-div">
-                            <Image className="StaticTruck" src="/../public/imgs/truck.svg" layout='fill'/>
+                            <Image className="StaticTruck" src="/public/imgs/truck.svg" width="450%" height="300%"/>
                     </div>
 
                         
