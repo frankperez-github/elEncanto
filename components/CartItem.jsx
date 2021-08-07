c9d3ed2a1030dd31991e0fb2306e4928d265ba7c
Adiazx14 <adrianvaldes1439@gmail.com>
2021-08-06 22:48:11 -0400

    added product links to cart  and order page

--- components/CartItem.jsx
+++ components/CartItem.jsx
@@ -1,15 +1,18 @@
 import { useContext } from "react"
 import { CartContext } from "../context/CartContext"
 import Image from 'next/image'
+import Link from 'next/link'
 
 const CartItem = ({item}) => {
     
     const {dispatch} = useContext(CartContext)
 
     return(
+        <Link href={`/product/${item.product}`}>
         <div className="cartItem">
+            
              <h1>{item.name} </h1>
-             <Image width={100} height={100} src={item.image}/>
+              <Image width={100} height={100} src={item.image}/> 
              <h2>Quantity: 
 
                  <select value={item.qty} onChange={(e)=>{dispatch({type:"ADD_ITEM", payload:{...item, qty:e.target.value}})}} name="" id="qty-select" className="qty-dropdown">
@@ -21,7 +24,10 @@ const CartItem = ({item}) => {
              <h2>Price: ${item.qty*item.price}</h2>
              
              <button className="buy_button" onClick={()=>{dispatch({type:"REMOVE_ITEM", payload:item})}}>Remove</button>
+            
+     
         </div>
+         </Link>
     )
 }
 
--- components/DeskVersion/OrderItem.jsx
+++ components/DeskVersion/OrderItem.jsx
@@ -1,10 +1,14 @@
 
 import Image from 'next/image'
+import Link from 'next/link'
+ 
+
 
 const OrderItem = ({item}) => {
     
 
     return(
+        <Link href={`/product/${item.product}`}>
         <div className="OrderItem">
              <h1>{item.name} </h1>
              <Image width={100} height={100} src={item.image}/>
@@ -13,6 +17,7 @@ const OrderItem = ({item}) => {
              <h2>Price: ${item.qty*item.price}</h2>
         
         </div>
+        </Link>
     )
 }
 
