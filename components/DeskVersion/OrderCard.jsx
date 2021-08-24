48bc397c4e7efb34da5a1261e133cb6d2af06c12
Adiazx14 <adrianvaldes1439@gmail.com>
2021-08-23 20:59:25 -0400

    trying to paypal

--- components/DeskVersion/OrderCard.jsx
+++ components/DeskVersion/OrderCard.jsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router"
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { UserContext } from "../../context/UserContext"
+import { PayPalButton } from "react-paypal-button-v2"
 import OrderItem from "./OrderItem"
 
 const OrderCard = ({order}) => {
@@ -10,7 +11,7 @@ const OrderCard = ({order}) => {
   const [sdk, setSdk] = useState(false)
 
     const deleteOrder = async() => {
-      await fetch(`https://elencanto-drf-api.herokuapp.com/orders/${order.id}/`, {method:"DELETE", headers:{"Content-Type": "application/json", authorization:`Bearer ${user.access}`}})
+      await fetch(`https://elencanto-drf-api.herokuapp.com/orders/${order.id}/`, {method:"DELETE", headers:{"Content-Type": "application/json", authorization:`Bearer ${user.access}`}, })
       router.reload(window.location.pathname)
       
     }
@@ -18,13 +19,30 @@ const OrderCard = ({order}) => {
 const addPaypalScript = ()=>{
   const script = document.createElement("script")
   script.type = 'text/javascript'
-  script.src = "https://www.paypal.com/sdk/js? client-id=AYfvlWFE-FyeSm2NBubForDud-H-5OAv98pe4_Y7-mqNSvbukVej_qzzcnumwu4IGf1gs7uASF2km1dp" 
+  script.src = "https://www.paypal.com/sdk/js?client-id=AYfvlWFE-FyeSm2NBubForDud-H-5OAv98pe4_Y7-mqNSvbukVej_qzzcnumwu4IGf1gs7uASF2km1dp"
   script.async = true
   script.onload = () => {
     setSdk(true)
   }
   document.body.appendChild(script)
 }
+
+const successPaymentHandler = async(paymentResult)  => {
+
+
+}
+
+
+
+useEffect(()=>{
+  if (!order.is_paid && order.is_approved)
+    
+  {
+    if (!window.script) addPaypalScript()
+    else setSdk(true)
+  }
+  
+},[user])
     return(
         <div className="orderCard">
                 <h4>Order id: {order.id}</h4>
@@ -36,9 +54,21 @@ const addPaypalScript = ()=>{
                {order.order_items.map(item=><OrderItem key={item.id} item={item}/>)}
              <h2>Shipping Price: ${order.shipping_price}</h2>
                <h2>Total Price: ${order.total_price}</h2>
-                {order.is_paid?<div><h5>`Paid at: {order.paid_at}`</h5> </div> :<button className="buy_button">Pay this Order</button>}
-                <h5>{order.is_delivered?`Delivered at ${order.delivered_at}`:"Not delivered yet"}</h5>
-                <button onClick= {deleteOrder} className="buy_button">Delete this order</button>
+                
+                
+                {!order.is_approved? <h1>Your order will be approved soon. Wait for an email from <a style={{color:"#FF524F", textDecoration:"underline"}}>elencantoliquidation@gmail.com</a> </h1>:
+                <div>
+                  <h1>Your order is approved</h1>
+                   {order.is_paid?<div><h5>`Paid at: {order.paid_at}`</h5> <h5>{order.is_delivered?`Delivered at ${order.delivered_at}`:"Not delivered yet"}</h5></div> :
+                 <div> <h1>Pay your order</h1>{sdk && <PayPalButton amount={order.total_price} onSuccess={async()=>{
+                  
+                  const res  = await fetch(`https://elencanto-drf-api.herokuapp.com/orders/${order.id}/`, {method:"PUT", headers:{"Content-Type": "application/json", authorization:`Bearer ${user.access}`},body:{"action":"pay"}})
+                  const data = await(res.json())
+                  console.log(data)
+                }}/>} </div> 
+    }
+                </div>}
+                <button onClick= {deleteOrder} className="buy_button">Cancel this order</button>
                 <hr />
                 <br />
                
